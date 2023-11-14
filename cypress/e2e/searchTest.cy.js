8daedb6fa8531c27109064f9b86d5de04d0821ae
Ye Kai Phang <38521895+yekai11@users.noreply.github.com>
2023-11-14 17:04:45 +0800

    changed test to open localhost:3000 instead

--- cypress/e2e/searchTest.cy.js
+++ cypress/e2e/searchTest.cy.js
@@ -17,7 +17,7 @@ describe("Search feature", () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit("http://localhost:3001");
+    cy.visit("http://localhost:3000");
   });
 
   it("displays default search page", () => {
